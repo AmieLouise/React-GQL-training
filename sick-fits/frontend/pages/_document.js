f1caf9def1293a17c96583f67f13e0db667f5c3e
AmieLouiseDooley <adooley@dunelm.com>
2022-10-29 18:21:36 +0100

    Stop style flickering on server render

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,7 +1,15 @@
 // import React from 'react'
 import Document, { Html, Head, NextScript, Main } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class _document extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet
+    const page = renderPage((App) => (props) =>  sheet.collectStyles(<App {...props} />))
+    const styleTags = sheet.getStyleElement()
+    
+    return {...page, styleTags}
+  }
     render() {
         return (
             <Html lang='en-GB' >
